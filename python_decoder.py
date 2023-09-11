2ec17702c35e37e2d83c3b3f381f3f37e7cdd02d
Shubham Ugare <shubhamdugare@gmail.com>
2023-09-11 12:35:52 -0500

    Write more tests

--- python_decoder.py
+++ python_decoder.py
@@ -1,4 +1,5 @@
 import string
+import time
 import torch
 from transformers import LogitsProcessor, PreTrainedTokenizer
 from lark import Lark
@@ -14,15 +15,37 @@ class PythonIndenter(Indenter):
 
 def get_token_type(parser, s):
     try:
+        if s == '\t':
+            return '_INDENT'
+        
+        # Non-regex direct matches
+        for t in parser.terminals:
+            if t.pattern.type == 'str' and t.pattern.value == s:
+                return t.name
+        
         interactive = parser.parse_interactive(s)
         token = next(interactive.iter_parse())
         return token.type
     except:
         return None
 
+def get_acceptable_next_tokens(parser, code):
+    interactive = parser.parse_interactive(code)
+    
+    for token in interactive.lexer_thread.lex(interactive.parser_state):
+            # print(repr(token))
+            if token.type == '_DEDENT':
+                continue
+            _ = interactive.feed_token(token)
+    
+    print('Acceptable Tokens:', interactive.accepts())
+    return interactive.accepts()
+
+
 class PythonDecoder(LogitsProcessor):
     
     def __init__(self, tokenizer: PreTrainedTokenizer, **kwargs):
+        time_start = time.time()
         self.tokenizer = tokenizer
         self.parser = Lark.open(
             "python_grammar.lark",
@@ -35,22 +58,55 @@ class PythonDecoder(LogitsProcessor):
         # Iterate through the vocabulary and create a map of (tokenizer token -> grammar terminal)
         # Note: It may happen that many tokens do not fall in any category
         self.token_to_terminal = {}
+        self.terminal_to_mask = {}
+        self.uncategorezed_mask = torch.zeros(tokenizer.vocab_size, dtype=torch.bool)
+
         for i in range(tokenizer.vocab_size):
-            token = tokenizer.decode(i)
+            token = tokenizer.decode(torch.tensor([i]), skip_special_tokens=True)
             token_type = get_token_type(self.parser, token)
+            
             if token_type is not None:
                 self.token_to_terminal[token] = token_type
+            
+                if not token_type in self.terminal_to_mask:
+                    self.terminal_to_mask[token_type] = torch.zeros(tokenizer.vocab_size, dtype=torch.bool)
+                self.terminal_to_mask[token_type][i] = True
+            else:
+                self.uncategorezed_mask[i] = False
         
         print(f"Found {len(self.token_to_terminal)}/{tokenizer.vocab_size} tokens that form a terminal.")
+        print("Time taken for preprocessing:", time.time() - time_start)
 
 
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
         partial_code = self.tokenizer.decode(input_ids[0], skip_special_tokens=True)
-        interactive = self.parser.parse_interactive(partial_code)
-        # feeds the text given to above into the parsers. This is not done automatically.
-        interactive.exhaust_lexer()
+
+        print("-"*80)
+        print('Code lenght:', len(partial_code))
+
+        print(partial_code)
+        print(repr(partial_code))
 
         # returns the names of the Terminals that are currently accepted.
-        print(interactive.accepts())
+        acceptable_token_types = get_acceptable_next_tokens(self.parser, partial_code)
+
+        accept_mask = self.uncategorezed_mask.clone()
+        # print(accept_mask.sum())
+
+        for token_type in acceptable_token_types:
+            if token_type in self.terminal_to_mask:
+                accept_mask |= self.terminal_to_mask[token_type]
+            # print(accept_mask.sum())
+        
+        print("Number of acceptable tokens:", accept_mask.sum().item())
+        print("-"*80)
+
+        greedy_token = self.tokenizer.decode(scores.argmax(dim=-1), skip_special_tokens=True)
+        print('Greedy token:', repr(greedy_token), scores.argmax(dim=-1))
+
+        scores = scores.masked_fill(~accept_mask.to(scores.device), -float("inf"))
+    
+        greedy_grammar_token = self.tokenizer.decode(scores.argmax(dim=-1), skip_special_tokens=True)
+        print('Greedy grammar-based token:', repr(greedy_grammar_token), scores.argmax(dim=-1))
         return scores
     
--- test_grammar.py
+++ test_grammar.py
@@ -1,6 +1,7 @@
+import torch
 from lark import Lark
 from lark.indenter import Indenter
-from python_decoder import get_token_type
+from python_decoder import get_acceptable_next_tokens, get_token_type
 from transformers import (
     LlamaTokenizer,
     LlamaForCausalLM,
@@ -8,6 +9,7 @@ from transformers import (
     PreTrainedTokenizer,
     LogitsProcessorList
 )
+
 
 class PythonIndenter(Indenter):
     NL_type = "_NL"
@@ -25,7 +27,29 @@ parser = Lark.open(
     propagate_positions=True,
 )
 
-def test_parser():
+tokenizer = LlamaTokenizer.from_pretrained("/share/models/llama_model/hf/7B")
+
+def test_vocab_terminals():
+    token_to_terminal = {}
+    token_type_count = {}
+
+    for i in range(tokenizer.vocab_size):
+        token = tokenizer.decode(i)
+        token_type = get_token_type(parser, token)
+        if token_type is not None:
+            token_to_terminal[token] = token_type
+
+            # Count the number of tokens of each type
+            if token_type not in token_type_count:
+                token_type_count[token_type] = 0
+
+            token_type_count[token_type] += 1
+
+    print(token_type_count)
+    print(f"Found {len(token_to_terminal)}/{tokenizer.vocab_size} tokens that form a terminal.")
+
+
+def test_parser1():
     code = f"""
 a = 3
 b = 4
@@ -48,30 +72,52 @@ def f():
     
     return sss
 """
+    get_acceptable_next_tokens(parser, code)
 
-    print(code)
-    interactive = parser.parse_interactive(code)
 
-    print(parser.parse(code))
+def test_parser2():
+    partial_code = 'from typing import List\n\n\ndef has_close_elements(numbers: List[float], threshold: float) -> bool:\n\t""" Check if in given list of numbers, are any two numbers closer to each other than\n\tgiven threshold.\n\t>>> has_close_elements([1.0, 2.0, 3.0], 0.5)\n\tFalse\n\t>>> has_close_elements([1.0, 2.8, 3.0, 4.0, 5.0, 2.0], 0.3)\n\tTrue\n\t"""\n\ta=3+5\n\tb='
+    get_acceptable_next_tokens(parser, partial_code)
 
-def test_vocab_terminals():
-    tokenizer = LlamaTokenizer.from_pretrained("/share/models/llama_model/hf/7B")
-    token_to_terminal = {}
-    token_type_count = {}
+def test_parser3():
+    partial_code = 'from typing import List\n\n\ndef has_close_elements(numbers: List[float], threshold: float) -> bool:\n\t""" Check if in given list of numbers, are any two numbers closer to each other than\n\tgiven threshold.\n\t>>> has_close_elements([1.0, 2.0, 3.0], 0.5)\n\tFalse\n\t>>> has_close_elements([1.0, 2.8, 3.0, 4.0, 5.0, 2.0], 0.3)\n\tTrue\n\t"""\n\tfor i in range(len(numbers) -1, -1, -1) :\n\t\tfor j in range(i+1, len(numbers) ,1) :\n\t\t\tif abs(numbers[i] - numbers[j] ) < threshold :\n\t\t\t\treturn True\n'
+    acc_tokens = get_acceptable_next_tokens(parser, partial_code)
+    print(acc_tokens)
+    # assert '_INDENT' in acc_tokens
 
-    for i in range(tokenizer.vocab_size):
-        token = tokenizer.decode(i)
-        token_type = get_token_type(parser, token)
-        if token_type is not None:
-            token_to_terminal[token] = token_type
+def test_parser4():
+    partial_code = 'from typing import List\n\n\ndef has_close_elements(numbers: List[float], threshold: float) -> bool:\n\t""" Check if in given list of numbers, are any two numbers closer to each other than\n\tgiven threshold.\n\t>>> has_close_elements([1.0, 2.0, 3.0], 0.5)\n\tFalse\n\t>>> has_close_elements([1.0, 2.8, 3.0, 4.0, 5.0, 2.0], 0.3)\n\tTrue\n\t"""\n\tfor i in range(len(numbers) -1, -1, -1) :\n\t\tfor j in range(i+1, len(numbers) ,1) :\n\t\t\tif abs(numbers[i] - numbers[j] ) < threshold :\n'
+    acc_tokens = get_acceptable_next_tokens(parser, partial_code)
+    assert '_INDENT' in acc_tokens
 
-            # Count the number of tokens of each type
-            if token_type not in token_type_count:
-                token_type_count[token_type] = 0
-            token_type_count[token_type] += 1
+def test_get_token_type():
+    assert get_token_type(parser, "\t") == "_INDENT"
+    assert get_token_type(parser, tokenizer.decode(torch.tensor([12]), skip_special_tokens=True)) == "_INDENT"
     
-    print(token_type_count)
-    print(f"Found {len(token_to_terminal)}/{tokenizer.vocab_size} tokens that form a terminal.")
+    assert get_token_type(parser, "\n") == "_NL"
+    assert get_token_type(parser, tokenizer.decode(torch.tensor([13]), skip_special_tokens=True)) == "_NL"
+    
+    assert get_token_type(parser, "def") == "DEF"
+    assert get_token_type(parser, "in") == "IN"
+    assert get_token_type(parser, "if") == "IF"
+    assert get_token_type(parser, "else") == "ELSE"
+    assert get_token_type(parser, "elif") == "ELIF"
+    assert get_token_type(parser, "for") == "FOR"
+    assert get_token_type(parser, "while") == "WHILE"
+    assert get_token_type(parser, "try") == "TRY"
+    assert get_token_type(parser, "except") == "EXCEPT"
+    assert get_token_type(parser, "finally") == "FINALLY"
+    assert get_token_type(parser, "with") == "WITH"
+    assert get_token_type(parser, "class") == "CLASS"
 
-test_vocab_terminals()
-test_parser()
+tests = [test_get_token_type, test_vocab_terminals, test_parser1, test_parser2, test_parser3, test_parser4]
+
+for test in tests:
+    print(f"Running test {test.__name__}")
+    try:
+        test()
+        print(f"Test {test.__name__} passed.")
+    except Exception as e:
+        print(f"Test {test.__name__} failed.")
+        print(e)
+    print("-"*80)
