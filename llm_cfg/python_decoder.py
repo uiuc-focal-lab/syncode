83382e3e2008c31328fcdbbca38f3aba8efecda7
Shubham Ugare <shubhamdugare@gmail.com>
2023-10-30 17:26:49 -0500

    Add multibatch generation with grammar mask

--- llm_cfg/python_decoder.py
+++ llm_cfg/python_decoder.py
@@ -17,7 +17,7 @@ class PythonDecoder(LogitsProcessor):
         self.inc_parsers = None
         
         # For backtracking to syntactically valid completions
-        self.partial_codes = []
+        self.partial_codes_trace = []
         self.last_valid_stage = 0
 
         # For profiling
@@ -34,20 +34,23 @@ class PythonDecoder(LogitsProcessor):
             
         print(f"Time taken for preprocessing: {time.time() - time_start:.2f}s")
 
+    def _print_current_status(self, partial_code, cur_ac_terminals, next_ac_terminals, incomplete_terminal):
+        print('partial code:\n', repr(partial_code))
+        print('inc:', repr(incomplete_terminal), '\n', 'cur:', cur_ac_terminals, '\n', 'next:', next_ac_terminals)
+
+
     def _reset(self):
         self.inc_parsers = [IncrementalParser() for _ in range(self.batch_size)]
-
+        self.last_valid_state = [0 for _ in range(self.batch_size)]
         self.accept_tokens_sizes = []
-        self.partial_codes = []
-        self.last_valid_stage = 0
+        self.partial_codes_trace = []
         self.token_cnt = 0
-        
         
 
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
         partial_codes = self.tokenizer.batch_decode(input_ids, skip_special_tokens=True)
 
-        if self.batch_size == None or (len(self.partial_codes) > 0 and self.partial_codes[-1] not in partial_codes[0]):
+        if self.batch_size == None or (len(self.partial_codes_trace) > 0 and self.partial_codes_trace[0] not in partial_codes[0]):
             self.batch_size = len(partial_codes)
             self._reset()
 
@@ -57,7 +60,7 @@ class PythonDecoder(LogitsProcessor):
         for i, partial_code in enumerate(partial_codes):
             try:
                 compilation_start_time = time.time()
-                self.partial_codes.append(partial_code)
+                self.partial_codes_trace.append(partial_code)
 
                 # returns the names of the Terminals that are currently accepted.
                 cur_ac_terminals, next_ac_terminals, incomplete_terminal = self.inc_parsers[i].get_acceptable_next_terminals(partial_code)
@@ -65,28 +68,31 @@ class PythonDecoder(LogitsProcessor):
                 greedy_token = self.tokenizer.decode(scores.argmax(dim=-1), skip_special_tokens=True) # For debugging - remove later
 
                 if 'EOF' in next_ac_terminals:
-                    self.last_valid_stage = len(input_ids[0])
+                    self.last_valid_state[i] = len(input_ids[i])
 
                 self.accept_tokens_sizes.append(len(cur_ac_terminals))  # For profiling
 
+                print(i, 'Time taken for compilation:', time.time() - compilation_start_time)
                 accept_mask = self.terminals_nfa.get_overapprox_tokens_mask(incomplete_terminal, next_ac_terminals)
 
-                print('partial code:\n', repr(partial_code))
-                print('inc:', repr(incomplete_terminal), '\n', 'cur:', cur_ac_terminals, '\n', 'next:', next_ac_terminals)
-
-                scores[i] = scores[i].masked_fill(~accept_mask.to(scores.device), -float("inf"))
-
+                print(i, 'Time taken for overapproximation:', time.time() - compilation_start_time)
                 if self.debug and self.token_cnt%50==0:
-                    print('Time taken for compilation:', time.time() - compilation_start_time)
+                    self._print_current_status(partial_code, cur_ac_terminals, next_ac_terminals, incomplete_terminal)
+                
+                if torch.sum(accept_mask) != 0: # If there are acceptable tokens for the current partial code 
+                    scores[i] = scores[i].masked_fill(~accept_mask.to(scores.device), -float("inf"))
+                else: # Otherwise, report the error and mask no tokens
+                    print('No acceptable tokens for the current partial code!')
+                    self._print_current_status(partial_code, cur_ac_terminals, next_ac_terminals, incomplete_terminal)
+
+                print(i, 'Time taken for masking:', time.time() - compilation_start_time)  
 
                 greedy_grammar_token = self.tokenizer.decode(scores.argmax(dim=-1), skip_special_tokens=True)
 
                 if greedy_token != greedy_grammar_token:
                     print('Greedy token:', repr(greedy_token), scores.argmax(dim=-1))
                     print('Greedy grammar-based token:', repr(greedy_grammar_token), scores.argmax(dim=-1))
-                    print('Current acceptable terminals:', cur_ac_terminals)
-                    print('Next acceptable terminals:', next_ac_terminals) 
-                    print('Partial code:', partial_code)
+                    self._print_current_status(partial_code, cur_ac_terminals, next_ac_terminals, incomplete_terminal)
                     self.non_matching_token_cnt += 1
             except Exception as e:
                 print("-"*80, '\n', 'Code lenght:', len(partial_code), '\n', partial_code, '\n', repr(partial_code), '\n', 'Error:', e)
