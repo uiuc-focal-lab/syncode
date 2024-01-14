85cd2fe0ed3cc9f0012ef87eba7487b7319738ed
hgKang02 <68604896+hgKang02@users.noreply.github.com>
2024-01-14 00:29:40 -0600

    Update infer.py
    add option for turning off the logger and also to choose max new tokens

--- llm_cfg/infer.py
+++ llm_cfg/infer.py
@@ -28,9 +28,11 @@ if __name__ == "__main__":
     p.add_argument("--quantize", type=bool, default=True)
     p.add_argument("--gpu", type=int, default=1)
     p.add_argument("--num_samples", type=int, default=1)
+    p.add_argument("--max_length", choices=[50, 100, 150, 200, 250, 300, 350, 400] ,type=int, default=400)
     p.add_argument("--language", choices = ["python", "go"], default = "python", help = "language")
     p.add_argument("--dataset", choices = ["mbxp", "multi-humaneval", "mathqa-x"], default = "multi-humaneval", help = "dataset")
     p.add_argument("--new_nfa", default=False, action='store_true')
+    p.add_argument("--no_logger", action='store_false', help="Disable logging")
     args = p.parse_args()
     num_samples_per_task = args.num_samples
     
@@ -47,11 +49,12 @@ if __name__ == "__main__":
         model = LlamaForCausalLM.from_pretrained(model_location, torch_dtype=torch.bfloat16).eval().to(device)
         out_dir = f"results/{args.model}/{args.language}/{args.dataset}/"
     
+
     out_path = out_dir + 'samples_' + str(num_samples_per_task) + '_mode_' + str(args.mode) + "_eval.jsonl"
     os.makedirs(out_dir, exist_ok=True)
     log_file = out_dir + 'logs/' + 'samples_' + str(num_samples_per_task) + '_mode_' + str(args.mode) + "_eval.log"
     os.makedirs(out_dir + 'logs/', exist_ok=True)
-    logger = common.Logger(log_file)
+    logger = common.Logger(log_file, args.no_logger)
     
     logit_processors = None
     if args.mode == 'grammar_mask':
@@ -66,7 +69,7 @@ if __name__ == "__main__":
         device=device, 
         logit_processors=logit_processors, 
         mode=args.mode, 
-        max_new_tokens=400,
+        max_new_tokens=args.max_length,
         language=args.language,
         )
 
