f1ff20a478f841643f4e57e6f1ce17b3a656abb4
hgKang02 <68604896+hgKang02@users.noreply.github.com>
2024-01-14 00:30:56 -0600

    add if statements to turn off the loggers.

--- llm_cfg/common.py
+++ llm_cfg/common.py
@@ -5,6 +5,8 @@ from grammars.python_parser import PythonIncrementalParser
 from grammars.go_parser import GoIncrementalParser
 from terminals_nfa import TerminalsNFA
 import time
+import json
+import sys
 
 # Remove this in future and add instruction to set the HF_CACHE env variable
 HF_CACHE = '/share/models/hugging_face/'
@@ -35,6 +37,8 @@ def load_nfa(language: str, tokenizer, inc_parser=None, use_cache=True):
     tokenizer_name = type(tokenizer).__name__
     nfa_dir = 'results/' + tokenizer_name + '/'
     nfa_path = nfa_dir + language + '_nfa.pkl'
+    nfa_time = nfa_dir + language + '_time.json'
+    os.makedirs(nfa_dir, exist_ok=True)
     start_time = time.time()
     if use_cache and os.path.exists(nfa_path):
         nfa = pickle.load(open(nfa_path, 'rb'))
@@ -49,12 +53,21 @@ def load_nfa(language: str, tokenizer, inc_parser=None, use_cache=True):
         exceptions = {}
         if language == 'python':
             exceptions = {'COMMENT': '#.*|\'\'\'.*?\'\'\'|""".*?"""/is', '_NL': '(\r?\n[\t ]*)+', 'LONG_STRING': '\'\'\'.*?\'\'\'|""".*?"""/is', 'STRING': '[ubf]?r?(".*?"|\'.*?\')'}
-        
-        os.makedirs(nfa_dir, exist_ok=True)
         nfa = TerminalsNFA(inc_parser.parser.terminals, vocab, exceptions=exceptions, special_token_ids=[tokenizer.eos_token_id])
         print(f'Time taken for creating NFA:', time.time() - start_time, flush=True)
-
+        NFA_time = time.time() - start_time
         pickle.dump(nfa, open(nfa_path, 'wb'))
+        sort_nfa = time.time() - start_time
+
+        time_dict = {
+            "Time taken for creating NFA " : NFA_time,
+            "Time taken for storing the NFA " : sort_nfa,
+            "The total memory of the lookup table is" : sys.getsizeof(nfa)
+        }
+
+        json_object = json.dumps(time_dict, indent=4)
+        with open(nfa_time, "w") as outfile:
+            outfile.write(json_object)
         print(f'Time taken for storing the NFA', time.time() - start_time, flush=True)
     return nfa
 
@@ -67,23 +80,36 @@ def create_parser(language):
             raise ValueError(f'Unknown language: {language}')
 
 class Logger:
-    def __init__(self, filename):
+    # is_logging_enabled = True  # Class variable to control logging
+
+    def __init__(self, filename, do_logger):
         self.filename = filename
         self.file = open(filename, 'w')
+        self.do_logger = do_logger
+
+    # @classmethod
+    # def set_logging_enabled(cls, enabled: bool):
+    #     cls.is_logging_enabled = enabled
 
     def log(self, msg):
-        self.file.write(msg + '\n')
-        self.file.flush()
-    
+        # if Logger.is_logging_enabled == True:
+        if self.do_logger == True:
+            self.file.write(msg + '\n')
+            self.file.flush()
+
     def log_code(self, msg, code):
-        self.file.write(msg + ':\n')
-        self.file.write('-'*80 + '\n')
-        self.file.write(code + '\n')
-        self.file.write('-'*80 + '\n')
-        self.file.flush()
+        # if Logger.is_logging_enabled == True:
+        if self.do_logger == True:
+            self.file.write(msg + ':\n')
+            self.file.write('-'*80 + '\n')
+            self.file.write(code + '\n')
+            self.file.write('-'*80 + '\n')
+            self.file.flush()
 
     def close(self):
-        self.file.close()
+        if self.file:
+            self.file.close()
+
 
 
 def run_tests(tests):
