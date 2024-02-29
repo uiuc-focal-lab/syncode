c788858d0a117cefa573894bbe49eee27f3706f2
hgKang02 <hangoo94@gmail.com>
2024-02-28 21:41:58 -0600

    Fixed some error

--- tests/test_grammar_python.py
+++ tests/test_grammar_python.py
@@ -3,7 +3,7 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../')
 from syncode.parsers.python_parser import PythonIncrementalParser
 from syncode.parsers import create_parser
-from syncode.common import run_tests
+import syncode.common
 from transformers import (
     LlamaTokenizer,
 )
