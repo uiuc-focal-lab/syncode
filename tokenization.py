67f007689f0e7a6fc349aa20400819d8084de9d9
Preston M. Firestone <firestone.preston@gmail.com>
2025-02-08 14:50:46 -0600

    Add mitigation for nohomomorphic tokenizers.
    For some reason the methods are behaving weirdly on my machine, so the method
    calls have explicit arguments for self.
    
    There's only the tests in the examples. I can work more on that.
    
    The purpose of this was to implement the algorithm for making ByteLevel
    tokenizers homomorphic again.

--- tokenization.py
+++ tokenization.py
@@ -1,5 +1,10 @@
 """tokenization.py: an example of a shim to turn bytelevel encoded strings to bytes and back. See tokenization.md for details."""
 
+from huggingface_hub import HfApi
+from transformers import AutoTokenizer
+from joblib import delayed, Parallel
+from functools import reduce, cache
+
 
 def bytes_to_unicode():
     """
@@ -26,6 +31,7 @@ def bytes_to_unicode():
     return dict(zip(bs, cs))
 
 
+@cache
 def enbyte(token: str) -> bytes:
     """Turn a token into the corresponding bytes.
 
@@ -43,8 +49,133 @@ def debyte(array: bytes) -> list[str]:
 
     Example:
     --------
-    >>> xdebyte(b'\xe2\x88')
+    >>> debyte(b'\xe2\x88')
     ['â', 'Ī']
     """
     byte_dict = bytes_to_unicode()
     return [byte_dict[b] for b in array]
+
+
+class ByteTokenizer(AutoTokenizer):
+    """A class to convert a tokenizer that works on code points to one that works on bytes. Expects a tokenizer that has a ByteLevel preprocessor."""
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def from_pretrained(self, model_id):
+        self.tokenizer = super().from_pretrained(model_id)
+        self.byte_vocab = dict(
+            map(lambda p: (enbyte(p[0]), p[1]), self.tokenizer.vocab.items())
+        )
+        self.vocab_byte = {v: k for k, v in self.byte_vocab.items()}
+        return self
+
+    def decode(self, token_ids: list[int]) -> bytes:
+        """Decode token_ids as bytes.
+
+        Examples:
+        ---------
+        >>> tok = ByteTokenizer.from_pretrained('gpt2')n
+        >>> tok.decode(tok, [19526, 254, 25001, 121, 28938, 245, 171, 120, 253]).decode()
+        '你好吗？'
+
+        >>> tok = ByteTokenizer.from_pretrained('gpt2')
+        >>> tok.decode(tok, [121, 254, 25001, 121, 28938, 245, 171])
+        b'\xbd\xa0\xe5\xa5\xbd\xe5\x90\x97\xef'
+        >>> '你好吗？'.encode()[1:-2]
+        b'\xbd\xa0\xe5\xa5\xbd\xe5\x90\x97\xef'
+        """
+        return reduce(
+            lambda p, q: p + q, map(lambda id: self.vocab_byte[id], token_ids)
+        )
+
+    def encode(self, byte_text: bytes) -> list[int]:
+        """Encode to token_ids, with bytes.
+
+        Examples:
+        ---------
+        >>> tok = ByteTokenizer.from_pretrained('gpt2')
+        >>> tok.encode(tok, '你好吗？'.encode())
+        [19526, 254, 25001, 121, 28938, 245, 171, 120]
+
+        >>> tok = ByteTokenizer.from_pretrained('gpt2')
+        >>> tok.encode(tok, '你好吗？'.encode()[1:-2])
+        [121, 254, 25001, 121, 28938, 245, 171]
+        """
+        prefix, text, postfix = self.split_bytes(byte_text)
+        return (
+            self.iterative_lookup(self, prefix)
+            + self.tokenizer.encode(text)
+            + self.iterative_lookup(self, postfix)
+        )
+
+    def split_bytes(byte_text: bytes) -> (bytes, str, bytes):
+        """Break the text into valid and invalid UTF-8.
+
+        Returns: (prefix, text, postfix) where text is the encoded valid text and prefix and postfix are invalid bytes.
+
+        Examples:
+        ---------
+        >>> split_bytes('你好吗？'.encode()[1:-2])
+        (b'\xbd\xa0', '好吗', b'\xef')
+        """
+        prefix = b""
+        postfix = b""
+        text = ""
+
+        try:
+            text = byte_text.decode()
+        except UnicodeDecodeError as err:
+            if err.start == 0:
+                # Failure began at first byte.
+                i = 0
+                while bin(err.object[i:][0])[2:4] == "10":
+                    # Scan forward to the next uft-8 start byte (doesn't begin with '10').
+                    i += 1
+                Prefix = byte_text[:i]
+                byte_text = byte_text[i:]
+
+        try:
+            text = byte_text.decode()
+        except UnicodeDecodeError as err:
+            postfix = byte_text[err.start :]
+            text = byte_text[: err.start].decode()
+
+        return prefix, text, postfix
+
+    def iterative_lookup(self, key: str) -> list[int]:
+        """Return the item or sequence of items key indexes in byte_vocab."""
+        items = []
+        while key:
+            # Repeat until key has become an empty string.
+            for i in range(len(key), 0, -1):
+                # Find largest prefix in dictionary.
+                try:
+                    items.append(self.byte_vocab[key[:i]])
+                except KeyError:
+                    continue
+                # If all went well, truncate key from the beginning.
+                key = key[i:]
+                break
+
+        return items
+
+
+api = HfApi()
+
+
+def check(model):
+    if not model.gated:
+        try:
+            tok = AutoTokenizer.from_pretrained(model.id)
+            return (model.id, "ByteLevel" in repr(tok.backend_tokenizer.pre_tokenizer))
+        except:
+            return (model.id, "failed")
+
+        else:
+            return (model.id, "gated")
+
+
+# byte_level =
+# Parallel(n_jobs=16)(delayed(check)(model) for model in api.list_models(task='text-generation'))
