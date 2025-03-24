2a3d49c932a7dce8ad601638ddbe59beb0494e0b
Shubham Ugare <shubhamdugare@gmail.com>
2025-03-24 13:16:59 -0500

    Bump version to v0.4.8

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requirements = [
 
 setuptools.setup(
     name="syncode",
-    version="0.4.7",
+    version="0.4.8",
     author="Shubham Ugare",
     author_email="shubhamugare@gmail.com",
     description="This package provides the tool for grammar augmented LLM generation.",
